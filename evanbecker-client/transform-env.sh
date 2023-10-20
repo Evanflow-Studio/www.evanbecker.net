a760e5171154901e9e4a1ffadaa3152dacc9d8f6
Evan Becker <ebecker@nvisia.com>
2023-10-19 22:52:25 -0500

    try transform-env file

---
+++ evanbecker-client/transform-env.sh
@@ -0,0 +1,5 @@
+echo -e "NEXT_PUBLIC_SITE_URL=${NEXT_PUBLIC_SITE_URL}" >> .env.local
+echo -e "AUTH0_DOMAIN=${AUTH0_DOMAIN}" >> .env.local
+echo -e "AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}" >> .env.local
+echo -e "AUTH0_AUDIENCE=${AUTH0_AUDIENCE}" >> .env.local
+echo -e "AUTH0_REDIRECT_URI=${AUTH0_REDIRECT_URI}" >> .env.local
