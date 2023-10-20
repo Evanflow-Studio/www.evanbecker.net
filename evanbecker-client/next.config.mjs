bbcae4de8b6d9ea45a8a02f7469fd6721827b0e3
Evan <me@evanbecker.net>
2023-10-19 22:52:25 -0500

    try transform-env file

--- evanbecker-client/next.config.mjs
+++ evanbecker-client/next.config.mjs
@@ -13,19 +13,6 @@ const withMDX = nextMDX({
         remarkPlugins: [remarkGfm],
         rehypePlugins: [rehypePrism],
     },
-})
-
-module.exports = {
-    serverRuntimeConfig: {
-        // Will only be available on the server side
-    },
-    publicRuntimeConfig: {
-        // Will be available on both server and client
-        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-        AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
-        AUTH0_REDIRECT_URI: process.env.AUTH0_REDIRECT_URI,
-    },
-}
+});
 
 export default withMDX(nextConfig)
