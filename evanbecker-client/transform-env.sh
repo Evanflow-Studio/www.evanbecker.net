4998557a953c25e130a4ef9ace3889c8198d6a0b
Evan <me@evanbecker.net>
2023-10-21 09:30:16 -0500

    Test

--- evanbecker-client/transform-env.sh
+++ evanbecker-client/transform-env.sh
@@ -1,3 +1,5 @@
+touch .env.local
+echo "Trying to push to .env.local"
 echo -e "NEXT_PUBLIC_SITE_URL=${NEXT_PUBLIC_SITE_URL}" >> .env.local
 echo -e "AUTH0_DOMAIN=${AUTH0_DOMAIN}" >> .env.local
 echo -e "AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}" >> .env.local
