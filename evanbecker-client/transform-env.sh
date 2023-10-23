11cd8e339642d0e68b7cb57744eb40cbf2b3ed92
Evan <me@evanbecker.net>
2023-10-22 19:41:47 -0500

    test

--- evanbecker-client/transform-env.sh
+++ evanbecker-client/transform-env.sh
@@ -2,7 +2,7 @@ touch .env.local
 echo "here's current env:"
 env
 echo "Trying to push to .env.local"
-echo -e "NEXT_PUBLIC_SITE_URL=${SITE_URL}" >> .env.local
+echo -e "NEXT_PUBLIC_SITE_URL2=${SITE_URL}" >> .env.local
 echo "printing .env.local"
 cat .env.local
 pwd
