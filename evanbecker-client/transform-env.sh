9095e129b7eccc0768eb5f39cc56adb1f5b98f53
Evan Becker <ebecker@nvisia.com>
2023-10-25 21:55:44 -0500

    fast test

--- evanbecker-client/transform-env.sh
+++ evanbecker-client/transform-env.sh
@@ -1,9 +1,8 @@
+rm .env.local
 touch .env.local
-echo "here's current env:"
-env
 echo "Trying to push to .env.local"
-#echo -e "NEXT_PUBLIC_SITE_URL2=${SITE_URL}" >> .env.local
-echo "printing .env.local"
-cat .env.local
-pwd
-ls -la
+echo -e "NEXT_PUBLIC_SITE_URL=${SITE_URL}" >> .env.local
+echo -e "AUTH0_DOMAIN=${AUTH0_DOMAIN}" >> .env.local
+echo -e "AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}" >> .env.local
+echo -e "AUTH0_AUDIENCE=${AUTH0_AUDIENCE}" >> .env.local
+echo -e "AUTH0_REDIRECT_URI=${AUTH0_REDIRECT_URI}" >> .env.local
