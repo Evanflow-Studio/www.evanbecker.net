7a0fc909f403ee949bf54d082955c17f91152d77
Evan <me@evanbecker.net>
2023-10-25 22:20:57 -0500

    fast test

--- evanbecker-client/transform-env.sh
+++ evanbecker-client/transform-env.sh
@@ -5,3 +5,5 @@ echo "Trying to push to .env.local"
 [[ ! -z "$AUTH0_CLIENT_ID" ]] && echo -e "AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID" >> .env.local
 [[ ! -z "$AUTH0_AUDIENCE" ]] && echo -e "AUTH0_AUDIENCE=$AUTH0_AUDIENCE" >> .env.local
 [[ ! -z "$AUTH0_REDIRECT_URI" ]] && echo -e "AUTH0_REDIRECT_URI=$AUTH0_REDIRECT_URI" >> .env.local
+
+echo "finished"
